66de5b8e7ca0ad3df2e05d1166d174573539f195
AcelestuZ <vierucatalin41@gmail.com>
2025-12-24 15:16:52 +0100

    Update 3TabB.lua

--- 3TabB.lua
+++ 3TabB.lua
@@ -6,7 +6,11 @@ BeastTab:CreateToggle({
     Name = "Custom Beast Crawl",
     CurrentValue = false,
     Callback = function(state)
-        local hum = lp.Character:FindFirstChildOfClass("Humanoid")
+        _G.BeastCrawl = state
+        local char = lp.Character
+        local hum = char and char:FindFirstChildOfClass("Humanoid")
+        if not hum then return end
+
         if state then 
             if not _G.cTrack then 
                 local a = Instance.new("Animation")
@@ -19,9 +23,32 @@ BeastTab:CreateToggle({
         else 
             hum.HipHeight = 0
             hum.WalkSpeed = 16
-            if _G.cTrack then 
-                _G.cTrack:Stop() 
-            end 
+            if _G.cTrack then _G.cTrack:Stop() end 
+        end
+    end
+})
+
+BeastTab:CreateToggle({
+    Name = "Anti-Jump Slowdown",
+    CurrentValue = false,
+    Callback = function(state)
+        _G.AntiJumpSlow = state
+        if state then
+            task.spawn(function()
+                while _G.AntiJumpSlow do
+                    local char = lp.Character
+                    local bPowers = char and char:FindFirstChild("BeastPowers")
+                    local pEvent = bPowers and bPowers:FindFirstChild("PowersEvent")
+                    
+                    if pEvent then
+                        local oldName = pEvent.Name
+                        pEvent.Name = "FakeEvent"
+                        repeat task.wait() until not _G.AntiJumpSlow or lp.Character ~= char
+                        pEvent.Name = oldName
+                    end
+                    task.wait(1)
+                end
+            end)
         end
     end
 })
