f997f38aef4b9e88c4a3bfc566bd0ef55cb5a903
AcelestuZ <vierucatalin41@gmail.com>
2025-12-24 10:49:16 +0100

    Update 3TabB.lua

--- 3TabB.lua
+++ 3TabB.lua
@@ -1,1 +1,27 @@
 -- Beast Tab
+local BeastTab = _G.HubWindow:CreateTab("Beast", 4483362458)
+local lp = game:GetService("Players").LocalPlayer
+
+BeastTab:CreateToggle({
+    Name = "Custom Beast Crawl",
+    CurrentValue = false,
+    Callback = function(state)
+        local hum = lp.Character:FindFirstChildOfClass("Humanoid")
+        if state then 
+            if not _G.cTrack then 
+                local a = Instance.new("Animation")
+                a.AnimationId = "rbxassetid://961932719"
+                _G.cTrack = hum:LoadAnimation(a) 
+            end 
+            hum.HipHeight = -2
+            hum.WalkSpeed = 8
+            _G.cTrack:Play()
+        else 
+            hum.HipHeight = 0
+            hum.WalkSpeed = 16
+            if _G.cTrack then 
+                _G.cTrack:Stop() 
+            end 
+        end
+    end
+})
